ad563becaf2445368b4a8587079be84ecdcdde59
joao emanuel soares pacheco <joaoe.pacheco@gmail.com>
2023-04-04 18:05:35 -0300

    feat: Implementa função word_search em word_search.py

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,26 @@
+def search_word(word, item):
+    text_lines = item["linhas_do_arquivo"]
+    occurrences = list()
+
+    for index in range(len(text_lines)):
+        if word in text_lines[index].lower():
+            occurrences.append({"linha": index + 1})
+
+    return occurrences
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    queue_items = [item for item in instance.queue]
+    response = []
+    for item in queue_items:
+        item_search_data = {
+            "palavra": word,
+            "arquivo": item["nome_do_arquivo"],
+            "ocorrencias": search_word(word, item)
+        }
+        if len(item_search_data["ocorrencias"]) > 0:
+            response.append(item_search_data)
+    return response
 
 
 def search_by_word(word, instance):
