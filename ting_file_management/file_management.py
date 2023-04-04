94e9d1b55f754baaedb42e2eb6ac150fa6999685
joao emanuel soares pacheco <joaoe.pacheco@gmail.com>
2023-04-04 15:51:18 -0300

    feat: Implementa função txt_importer em file_management.py

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file.endswith(".txt"):
+        try:
+            with open(path_file, "r") as file:
+                data = file.read()
+                return data.split("\n")
+        except FileNotFoundError:
+            sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+    else:
+        sys.stderr.write("Formato inválido")
