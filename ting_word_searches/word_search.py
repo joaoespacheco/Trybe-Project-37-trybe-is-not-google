ee8b0c674d894110983e7cc8a481dbcaaed2a4ee
joao emanuel soares pacheco <joaoe.pacheco@gmail.com>
2023-04-04 18:17:26 -0300

    feat: Implementa a função search_by_word em word_search.py

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,10 +1,24 @@
-def search_word(word, item):
+def search_word_line(word, item):
     text_lines = item["linhas_do_arquivo"]
     occurrences = list()
 
     for index in range(len(text_lines)):
         if word in text_lines[index].lower():
             occurrences.append({"linha": index + 1})
+
+    return occurrences
+
+
+def search_word_line_and_content(word, item):
+    text_lines = item["linhas_do_arquivo"]
+    occurrences = list()
+
+    for index in range(len(text_lines)):
+        if word in text_lines[index].lower():
+            occurrences.append({
+                "linha": index + 1,
+                "conteudo": text_lines[index],
+                })
 
     return occurrences
 
@@ -16,7 +30,7 @@ def exists_word(word, instance):
         item_search_data = {
             "palavra": word,
             "arquivo": item["nome_do_arquivo"],
-            "ocorrencias": search_word(word, item)
+            "ocorrencias": search_word_line(word, item)
         }
         if len(item_search_data["ocorrencias"]) > 0:
             response.append(item_search_data)
@@ -24,4 +38,14 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    queue_items = [item for item in instance.queue]
+    response = []
+    for item in queue_items:
+        item_search_data = {
+            "palavra": word,
+            "arquivo": item["nome_do_arquivo"],
+            "ocorrencias": search_word_line_and_content(word, item)
+        }
+        if len(item_search_data["ocorrencias"]) > 0:
+            response.append(item_search_data)
+    return response
